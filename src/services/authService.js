37fed77873c5c94be7e0733e311b1f434423eb6c
Steven Falcones León <140020193+fsteven96@users.noreply.github.com>
2025-06-16 15:44:50 -0500

    Update authService.js

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5232/api/auth';
+const API_URL = 'https://peluqueriaanita-aqegegejepffavg3.canadacentral-01.azurewebsites.net/api/auth';
 
 export const login = async (UsuarioNombre, PasswordU) => {
   const response = await axios.post(`${API_URL}/login`, {
