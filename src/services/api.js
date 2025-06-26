623d6080fa231a9343005bdc4d3e00962aa3de57
Steven Falcones León <140020193+fsteven96@users.noreply.github.com>
2025-06-25 22:16:07 -0500

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,15 +1,15 @@
 import axios from 'axios';
 const API = 'https://api-anita-peluqueria-fthph6f5ebd8f5bp.brazilsouth-01.azurewebsites.net/api';
 
-export const obtenerClientes = async () => (await axios.get(`${API}/Cliente/todosClientes`)).data;
-export const crearCliente = async (cliente) => await axios.post(`${API}/Cliente/agregarCliente`, cliente);
+export const obtenerClientes = async () => (await axios.get(`${API}/todosClientes`)).data;
+export const crearCliente = async (cliente) => await axios.post(`${API}/agregarCliente`, cliente);
 
-export const obtenerCitasPorCliente = async () => (await axios.get(`${API}/Cita/activas`)).data;
+export const obtenerCitasPorCliente = async () => (await axios.get(`${API}/citas/activas`)).data;
 export const agendarCita = async (cita) => {
   console.log('Enviando cita al backend:', cita); 
-  return await axios.post(`${API}/Cita`, cita);
+  return await axios.post(`${API}/citas`, cita);
 };
 
-export const obtenerAtenciones = async (clienteId) => (await axios.get(`${API}/atencion`)).data;
-export const registrarAtencion = async (atencion) => await axios.post(`${API}/atencion`, atencion);
+export const obtenerAtenciones = async (clienteId) => (await axios.get(`${API}/atenciones`)).data;
+export const registrarAtencion = async (atencion) => await axios.post(`${API}/atenciones`, atencion);
 
